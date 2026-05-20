6ceedc060a46c7b278d3dcf63a0b0a36426d601d
pauortec <pauortec@gmail.com>
2026-05-20 20:47:45 -0300

    fix: corregir nombre del módulo de gestion_tareas a core

--- app/core/settings.py
+++ app/core/settings.py
@@ -50,7 +50,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'gestion_tareas.urls'
+ROOT_URLCONF = 'core.urls'
 
 TEMPLATES = [
     {
@@ -67,7 +67,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'gestion_tareas.wsgi.application'
+WSGI_APPLICATION = 'core.wsgi.application'
 
 
 # Database
