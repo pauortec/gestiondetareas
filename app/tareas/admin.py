486b11a35457484379845f530b7e009db007aeb7
IsaiasAlmiron <isaias.almiron@sati.com.py>
2026-05-30 09:44:53 -0300

    chore: generar migracion del nuevo esquema y registrar modelos en el admin

--- app/tareas/admin.py
+++ app/tareas/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Proyecto, Tarea, ParteHoras, Notificacion
 
-# Register your models here.
+admin.site.register(Proyecto)
+admin.site.register(Tarea)
+admin.site.register(ParteHoras)
+admin.site.register(Notificacion)
